0983d0ea5313df5301bddf765eed820a1d30c9b0
unknown <vithianna.lu@gmail.com>
2019-06-27 17:49:40 -0300

    Tela de cadastro

--- app/src/main/java/com/example/anotacoes/ui/CadastroActivity.kt
+++ app/src/main/java/com/example/anotacoes/ui/CadastroActivity.kt
@@ -7,6 +7,7 @@ import com.example.anotacoes.data.model.Usuario
 import com.example.anotacoes.utils.LocalDbImplement
 import kotlinx.android.synthetic.main.activity_cadastro.*
 import kotlinx.android.synthetic.main.activity_main.*
+import org.jetbrains.anko.email
 import org.jetbrains.anko.toast
 
 class CadastroActivity : AppCompatActivity() {
@@ -17,28 +18,79 @@ class CadastroActivity : AppCompatActivity() {
 
         salvar_cadastro.setOnClickListener {
 
-            var usuarios: List<Usuario> = listOf()
-            if(LocalDbImplement<Usuario>(this@CadastroActivity).getList(Usuario::class.java, "usuarios") != null){
-                usuarios = LocalDbImplement<Usuario>(this@CadastroActivity).getList(Usuario::class.java, "usuarios")!!
-            }
-            val usuariosList: ArrayList<Usuario> = ArrayList(usuarios)
-
-            val usuario = Usuario()
-            usuario.nick = nick.text.toString()
-            usuario.nome = nome.text.toString()
-            usuario.email = email_login.text.toString()
-            usuario.senha = senha_login.text.toString()
-
-            usuariosList.add(usuario)
-
-            LocalDbImplement<Usuario>(this@CadastroActivity).saveList(usuariosList, Usuario::class.java, "usuarios")
-
-            toast("Usuário cadastro com sucesso!")
-            finish()
+            val resultado = validar()
+            requestLogin(resultado)
         }
 
         cancelar_cadastro.setOnClickListener {
+            //Corta a tela atual
             finish()
+
         }
     }
+
+    private fun validar(): Boolean {
+
+        if(senha_login.length()<4){
+           toast("A senha deve ter pelo menos 8 digítos")
+            return false
+        }
+
+        if(!(senha_login.text.toString().equals(confirmar_senha.text.toString()))){
+            toast("As senhas devem corresponder")
+            return false
+        }
+
+
+
+
+
+        if (nick.text.toString().isEmpty()) {
+            nick.error = "Campo obrigatorio";
+            nick.requestFocus()
+            return false
+        }
+
+        if (nome.text.toString().isEmpty()) {
+            nome.error = "Campo obrigatorio";
+            nome.requestFocus()
+            return false
+        }
+        if (email_login.text.toString().isEmpty()) {
+            email_login.error = "Campo obrigatorio";
+            email_login.requestFocus()
+            return false
+        }
+
+        return true
+    }
+
+    fun requestLogin(login: Boolean){
+ if(login) {
+    var usuarios: List<Usuario> = listOf()
+    //Get List
+    if (LocalDbImplement<Usuario>(this@CadastroActivity).getList(Usuario::class.java, "usuarios") != null) {
+        usuarios = LocalDbImplement<Usuario>(this@CadastroActivity).getList(Usuario::class.java, "usuarios")!!
+    }
+    val usuariosList: ArrayList<Usuario> = ArrayList(usuarios)
+
+    val usuario = Usuario()
+    usuario.nick = nick.text.toString()
+    usuario.nome = nome.text.toString()
+    usuario.email = email_login.text.toString()
+    usuario.senha = senha_login.text.toString()
+
+    usuariosList.add(usuario)
+
+    //Save List
+    LocalDbImplement<Usuario>(this@CadastroActivity).saveList(usuariosList, Usuario::class.java, "usuarios")
+
+    toast("Usuário cadastro com sucesso!")
+    //Corta a tela atual
+    finish()
+}
+
+    }
+
+
 }
--- app/src/main/java/com/example/anotacoes/ui/MainActivity.kt
+++ app/src/main/java/com/example/anotacoes/ui/MainActivity.kt
@@ -1,19 +1,25 @@
 package com.example.anotacoes.ui
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import com.example.anotacoes.R
 import com.example.anotacoes.data.model.Usuario
 import com.example.anotacoes.utils.LocalDbImplement
+import kotlinx.android.synthetic.main.activity_cadastro.*
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlin.coroutines.EmptyCoroutineContext
+
 
 class MainActivity : AppCompatActivity() {
+
 
     //BOTÃO DE LOGIN <INICIO>
     //variavel de valor unico com seu nome será igual a um array do tipo class.
     private var pessoas: List<Usuario> = listOf()
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,7 +27,7 @@ class MainActivity : AppCompatActivity() {
 
         if(LocalDbImplement<Usuario>(this@MainActivity).getList(Usuario::class.java, "usuarios") != null){
             pessoas = LocalDbImplement<Usuario>(this@MainActivity).getList(Usuario::class.java, "usuarios")!!
-        }
+    }
 
         btnLogin.setOnClickListener {
 
@@ -29,10 +35,11 @@ class MainActivity : AppCompatActivity() {
 
             while (x < pessoas.size) {
 
-                if (email.text.toString() == pessoas[x].email && senha.text.toString() == pessoas[x].senha) {
+                if (email.text.toString().equals(pessoas[x].email) && senha.text.toString().equals(pessoas[x].senha)) {
                     val intent = Intent(this, MenuActivity::class.java)
                     startActivity(intent)
                     break
+
                 }
                 if (x == pessoas.size - 1) {
                     Toast.makeText(this, "Senha ou Email invalido", Toast.LENGTH_SHORT).show()
@@ -48,8 +55,9 @@ class MainActivity : AppCompatActivity() {
             val Intent = Intent(this, CadastroActivity::class.java)
             startActivity(Intent)
         }
-        }
+        //BOTÃO DE CADASTRO <FIM>
     }
+}
 
 
 
